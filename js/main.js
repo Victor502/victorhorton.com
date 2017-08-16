d336e66402daaa4f10a319469f5d415f459b6b2c
Victor Horton <victorahorton@yahoo.com>
2017-08-16 17:38:51 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".sticky").sticky({topSpacing:0});
+  });
+
+$('.sticky').sticky({
+  responsiveWidth: true
+});
+
+/*$('.sticky').on('sticky-start', function() {
+  $(this).append('');
+});
+
+$('.sticky').on('sticky-end', function() {
+    $('.email-text').remove();
+});*/
